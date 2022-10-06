e2d125cb0946bd19571a6535c082fbf77059ab31
Felipe Molano <molano5858@gmail.com>
2022-10-05 21:39:25 -0500

    Clases 9 y 10 render de calavera y los movimientos con flechas y botones funcionen

--- game.js
+++ game.js
@@ -3,6 +3,17 @@ const game=canvas.getContext('2d');
 let canvasSize;
 let elementSize;
 
+
+let up=document.querySelector('#up') ;
+let left=document.querySelector('#left') ;
+let right=document.querySelector('#right') ;
+let down=document.querySelector('#down') ;
+
+let playerPosition={
+    x:undefined,
+    y:undefined,
+}
+
 window.addEventListener('load',setCanvasSize);// le estamos diciendo que cuanto la ventana cargue, ejecute la funcion startGame
 window.addEventListener('resize',setCanvasSize)// evento resize de window nos permite ejecutar la funcion cada vez que se hace cambio dinamico de la pagina, esto soluciona el hecho de que el canva no cambiaba de tamaño si no solo cuando recargabamos
 
@@ -37,7 +48,7 @@ function startGame(){
     game.font=(elementSize-10)+'px Verdana';//estamos haciendo el emoji del tamaño de cada espacio en el canvas que quedamos era 10x10
     game.textAlign='end';
 
-    let map=maps[2];//[]la posicion de map va a ser el mapa, hay solo 3 mapas
+    let map=maps[0];//[]la posicion de map va a ser el mapa, hay solo 3 mapas
     let mapRows=map.trim().split('\n');// .trim() elimina los espacios en blanco al inicio y al final de un string, .split() convierte a un array un string y cada posicion o elemento del array vendra dado por el parametro que le pasemos,
     console.log(mapRows);
     
@@ -56,40 +67,61 @@ function startGame(){
     //     }
     // } 
 
+
+    clearMap()// las calaveras cada que las movemos se duplican, para ello digamos que borramos todo el mapa y luego lo renderizamos de nuevo
+
     //vamos a escribir el anterior for de una manera mas legible y mas "profesional"
     mapRowCols.forEach((row, rowI)=>{ // el forEach puede devolver el indice, tradicionalmente siempre he usado un parametro, pero si le ponemos dos, el segundo representa el indice
         row.forEach((col,colI)=>{
             const emoji=emojis[col];
             const posX = elementSize*(colI+1);
             const posY = elementSize*(rowI+1);
+
+            //vamos a renderizar la calaberita, osea el jugador en la misma posicion de la puerta
+
+            if(col=='O'){//O es el key que representa la puerta
+                if(!playerPosition.x && !playerPosition.y){//como estamos borrando el mapa cada vez aqui estamos cubriendo al render diciendo que solo renderice la calabera en la posicion inicial la primera vez, porque en la segunda o demas ya positionPlayer.x y y no seran undefined
+                playerPosition.x=posX;
+                playerPosition.y=posY;
+                }
+            }
             game.fillText(emoji,posX,posY);
+            
+            
             // console.log({row, rowI, col, colI})
         })
     })
+    
 
-
-
-
+    movePlayer()
     
     
 }
 
+function clearMap(){
+    game.clearRect(0,0,canvasSize,canvasSize)
+}
+
+function movePlayer(){
+    
+    const emojiPlayer=emojis['PLAYER'];
+    game.fillText(emojiPlayer,playerPosition.x,playerPosition.y);
+
+}
+
 //EVENTOS DE LOS BOTONES DE DIRECCION
 
-let up=document.querySelector('#up') ;
-let left=document.querySelector('#left') ;
-let right=document.querySelector('#right') ;
-let down=document.querySelector('#down') ;
+
 
 up.addEventListener('click',moverArriba);
 left.addEventListener('click',moverIzquierda);
 right.addEventListener('click',moverDerecha);
 down.addEventListener('click',moverAbajo);
 
-up.addEventListener('keydown',moveByKey);
-left.addEventListener('keydown',moveByKey);
-right.addEventListener('keydown',moveByKey);
-down.addEventListener('keydown',moveByKey);
+window.addEventListener('keydown',moveByKey);
+window.addEventListener('keydown',moveByKey);
+window.addEventListener('keydown',moveByKey);
+window.addEventListener('keydown',moveByKey);
 
 
 
@@ -101,18 +133,27 @@ down.addEventListener('keydown',moveByKey);
 
 
 function moverArriba(){
-    console.log('arriba')
+    playerPosition.y-=elementSize;
+    startGame()
+    console.log('Arriba');
+
 };
 
 function moverIzquierda(){
+    playerPosition.x-=elementSize;
+    startGame()
     console.log('Izquierda')
 }
 
 function moverDerecha(){
+    playerPosition.x+=elementSize;
+    startGame()
     console.log('Derecha')
 };
 
 function moverAbajo(){
+    playerPosition.y+=elementSize;
+    startGame()
     console.log('Abajo')
 }
 
